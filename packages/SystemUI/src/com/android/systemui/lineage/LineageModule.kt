0f42a6852c0f781d24d2b72cbc396e3e55810ec7
Adin Kwok <adin.kwok@carbonrom.org>
2018-04-07 20:08:45 -0700

    SystemUI: Add Smart Pixels tile
    Single tap enables/disables.
    Long press opens Smart Pixels settings.
    
    User is not allowed to enable or disable when
    Smart Pixels has been auto-enabled on battery saver.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 7 Jul 2019 11:56:12 +0530
        Subject: Fix long click intent for Smart Pixels tile [1/2]
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I535c84a0ca6360e1db351391e9d0fb9095896ee3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.RefreshRateTile
 import com.android.systemui.qs.tiles.ScreenshotTile
+import com.android.systemui.qs.tiles.SmartPixelsTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
@@ -170,6 +171,12 @@ interface LineageModule {
     @StringKey(ScreenshotTile.TILE_SPEC)
     fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
 
+    /** Inject SmartPixelsTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SmartPixelsTile.TILE_SPEC)
+    fun bindSmartPixelsTile(smartPixelsTile: SmartPixelsTile): QSTileImpl<*>
+
     /** Inject SoundTile into tileMap in QSModule */
     @Binds
     @IntoMap
@@ -293,6 +300,21 @@ interface LineageModule {
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.CONNECTIVITY
+            )
+        }
+
+        @Provides
+        @IntoMap
+        @StringKey(SmartPixelsTile.TILE_SPEC)
+        fun provideSmartPixelsTileConfig(uiEventLogger: QsEventLogger): QSTileConfig {
+            return QSTileConfig(
+                tileSpec = TileSpec.create(SmartPixelsTile.TILE_SPEC),
+                uiConfig = QSTileUIConfig.Resource(
+                    iconRes = R.drawable.ic_qs_smart_pixels,
+                    labelRes = R.string.quick_settings_smart_pixels
+                ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.DISPLAY
             )
         }
 
