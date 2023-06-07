5fd7bf5c7504cf3bbe4391b3031c1c9338b5d1a1
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-07 23:59:41 +0300

    base: services: Introduce AutoDimService [1/2]
    Exactly like AutoAODService but for REDUCE_BRIGHT_COLORS_ACTIVATED

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -309,5 +309,12 @@ public class SecureSettings {
         Settings.Secure.SELECTED_SPELL_CHECKER_SUBTYPE,
         // Evolution X additions
         Settings.Secure.VPN_ENFORCE_DNS,
+<<<<<<< HEAD
+=======
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
+        Settings.Secure.EXTRA_DIM_AUTO_MODE,
+        Settings.Secure.EXTRA_DIM_AUTO_TIME,
+>>>>>>> cf7f213498fd (base: services: Introduce AutoDimService [1/2])
     };
 }
