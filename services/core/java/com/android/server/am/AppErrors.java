c366ff856a613c8dd0a0f6b4822f386b59b5476e
NurKeinNeid <mralexman3000@gmail.com>
2024-06-15 15:10:14 +0200

    core: Switch to mkrbin for copying crash logs
    Change-Id: I388a4b98e7a10ebba32cdbe4c98a62c340b902d1
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -674,7 +674,7 @@ class AppErrors {
                 return;
             }
 
-            // Add paste content for Memochō option
+            // Add paste content for MkrBin option
             String devFP = SystemProperties.get("ro.vendor.build.fingerprint", "");
             String everestVers = SystemProperties.get("ro.everest.version", "");
             String boardName = SystemProperties.get("ro.product.board", "");
