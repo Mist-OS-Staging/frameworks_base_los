9b005d159d0f63abbd6a37f136a648b91fdcdccc
PK Guo <pk.guo@mediatek.com>
2025-04-22 14:23:59 +0800

    audio: fix SAD and SAM inconsistent after audioserver crash
    When restoring setting after audioserver crash
    1. The setHdmiSystemAudioSupported doesn't setForceUse with SAM
    because the setting value is equal to mHdmiSystemAudioSupported.
    2. The SAD is stored in the AudioDescriptor, but AudioDescriptor
    isn't pass to native layer.
    
    Adjust logic to ensure that SAM and AudioDescriptor can set to
    native layer when restoring setting after the crash.
    
    Bug: 412816152
    Test: manual
    Change-Id: I3a473dddfd4e69a4dbfe0905c2ba0aa849c8cfae

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -2154,7 +2154,7 @@ public class AudioService extends IAudioService.Stub
         }
         synchronized (mHdmiClientLock) {
             if (mHdmiManager != null && mHdmiTvClient != null) {
-                setHdmiSystemAudioSupported(mHdmiSystemAudioSupported);
+                setHdmiSystemAudioSupported(mHdmiSystemAudioSupported, true);
             }
         }
 
@@ -12745,6 +12745,10 @@ public class AudioService extends IAudioService.Stub
 
     @Override
     public int setHdmiSystemAudioSupported(boolean on) {
+        return setHdmiSystemAudioSupported(on, false);
+    }
+
+    private int setHdmiSystemAudioSupported(boolean on, boolean force) {
         int device = AudioSystem.DEVICE_NONE;
         synchronized (mHdmiClientLock) {
             if (mHdmiManager != null) {
@@ -12753,7 +12757,7 @@ public class AudioService extends IAudioService.Stub
                             + "system audio mode.");
                     return device;
                 }
-                if (mHdmiSystemAudioSupported != on) {
+                if (force || mHdmiSystemAudioSupported != on) {
                     mHdmiSystemAudioSupported = on;
                     final int config = on ? AudioSystem.FORCE_HDMI_SYSTEM_AUDIO_ENFORCED :
                         AudioSystem.FORCE_NONE;
