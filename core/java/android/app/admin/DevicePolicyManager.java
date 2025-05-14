TheParasiteProject/frameworks_base

base: Spoof current storage encryption status [1/2]
* Some apps like Health Connect wants device to be encrypted
* Spoof cyrpto state to encrypted while disabling encryption possible

Change-Id: Ied3e080f3c91da861ea955692b991e6aee0946d4
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

core/java/android/app/admin/DevicePolicyManager.java
@@ -7121,6 +7121,10 @@ public class DevicePolicyManager {
     /** @hide per-user version */
     @UnsupportedAppUsage
     public int getStorageEncryptionStatus(int userHandle) {
+        if (Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.SPOOF_STORAGE_ENCRYPTION_STATUS, 0) == 1) {
+            return ENCRYPTION_STATUS_ACTIVE;
+        }
         if (mService != null) {
             try {
                 return mService.getStorageEncryptionStatus(mContext.getPackageName(), userHandle);

Mist-OS-Staging/frameworks_base_los

core/java/android/app/admin/DevicePolicyManager.java
@@ -7494,6 +7494,10 @@ public class DevicePolicyManager {
     /** @hide per-user version */
     @UnsupportedAppUsage
     public int getStorageEncryptionStatus(int userHandle) {
+        if (Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.SPOOF_STORAGE_ENCRYPTION_STATUS, 0) == 1) {
+            return ENCRYPTION_STATUS_ACTIVE;
+        }
         if (mService != null) {
             try {
                 return mService.getStorageEncryptionStatus(mContext.getPackageName(), userHandle);