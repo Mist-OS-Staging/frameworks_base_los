israfilbd/android_frameworks_base

services: Introduce Reality Display engine [1/2]
* inspired by sony's vivid/x-reality display modes

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

services/core/java/com/android/server/display/color/ColorDisplayService.java
@@ -116,6 +116,7 @@ public final class ColorDisplayService extends SystemService {
     private static final int MSG_APPLY_GLOBAL_SATURATION = 4;
     private static final int MSG_APPLY_DISPLAY_WHITE_BALANCE = 5;
     private static final int MSG_APPLY_REDUCE_BRIGHT_COLORS = 6;
+    private static final int MSG_APPLY_UPDATE_DISPLAY_ENGINE = 7;
 
     /**
      * Return value if a setting has not been set.
@@ -161,6 +162,8 @@ public final class ColorDisplayService extends SystemService {
             new GlobalSaturationTintController();
     private final ReduceBrightColorsTintController mReduceBrightColorsTintController =
             new ReduceBrightColorsTintController();
+    private final DisplayEngineController mDisplayEngineController =
+            new DisplayEngineController();
 
     @VisibleForTesting
     final Handler mHandler;
@@ -367,6 +370,9 @@ public final class ColorDisplayService extends SystemService {
                                 onReduceBrightColorsStrengthLevelChanged();
                                 mHandler.sendEmptyMessage(MSG_APPLY_REDUCE_BRIGHT_COLORS);
                                 break;
+                            case DisplayEngineController.DISPLAY_ENGINE_MODE:
+                                mHandler.sendEmptyMessage(MSG_APPLY_UPDATE_DISPLAY_ENGINE);
+                                break;
                         }
                     }
                 }
@@ -397,6 +403,8 @@ public final class ColorDisplayService extends SystemService {
         cr.registerContentObserver(Secure.getUriFor(Secure.REDUCE_BRIGHT_COLORS_ACTIVATED),
                 false /* notifyForDescendants */, mContentObserver, mCurrentUser);
         cr.registerContentObserver(Secure.getUriFor(Secure.REDUCE_BRIGHT_COLORS_LEVEL),
+                false /* notifyForDescendants */, mContentObserver, mCurrentUser);
+        cr.registerContentObserver(Secure.getUriFor(DisplayEngineController.DISPLAY_ENGINE_MODE),
                 false /* notifyForDescendants */, mContentObserver, mCurrentUser);
 
         // Apply the accessibility settings first, since they override most other settings.
@@ -444,6 +452,10 @@ public final class ColorDisplayService extends SystemService {
                 onReduceBrightColorsActivationChanged(/*userInitiated*/ false);
                 mHandler.sendEmptyMessage(MSG_APPLY_REDUCE_BRIGHT_COLORS);
             }
+        }
+        
+        if (mDisplayEngineController.isAvailable(getContext())) {
+            mHandler.sendEmptyMessage(MSG_APPLY_UPDATE_DISPLAY_ENGINE);
         }
     }
 
@@ -1692,6 +1704,10 @@ public final class ColorDisplayService extends SystemService {
                     break;
                 case MSG_APPLY_DISPLAY_WHITE_BALANCE:
                     applyTintByCct(mDisplayWhiteBalanceTintController, false);
+                    break;
+                case MSG_APPLY_UPDATE_DISPLAY_ENGINE:
+                    mDisplayEngineController.updateBalance(getContext(), mCurrentUser);
+                    applyTint(mDisplayEngineController, true);
                     break;
             }
         }

Mist-OS-Staging/frameworks_base_los

Change-Id: I513456202ac3e872e59b932656beccb561d0dc91
Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

services/core/java/com/android/server/display/color/ColorDisplayService.java
@@ -125,6 +125,7 @@ public final class ColorDisplayService extends SystemService {
     private static final int MSG_APPLY_GLOBAL_SATURATION = 4;
     private static final int MSG_APPLY_DISPLAY_WHITE_BALANCE = 5;
     private static final int MSG_APPLY_REDUCE_BRIGHT_COLORS = 6;
+    private static final int MSG_APPLY_UPDATE_DISPLAY_ENGINE = 7;
 
     /**
      * Return value if a setting has not been set.
@@ -170,6 +171,8 @@ public final class ColorDisplayService extends SystemService {
             new GlobalSaturationTintController();
     private final ReduceBrightColorsTintController mReduceBrightColorsTintController =
             new ReduceBrightColorsTintController();
+    private final DisplayEngineController mDisplayEngineController =
+            new DisplayEngineController();
 
     @VisibleForTesting
     final Handler mHandler;
@@ -389,6 +392,9 @@ public final class ColorDisplayService extends SystemService {
                                 onReduceBrightColorsStrengthLevelChanged();
                                 mHandler.sendEmptyMessage(MSG_APPLY_REDUCE_BRIGHT_COLORS);
                                 break;
+                            case Secure.DISPLAY_ENGINE_MODE:
+                                mHandler.sendEmptyMessage(MSG_APPLY_UPDATE_DISPLAY_ENGINE);
+                                break;
                         }
                     }
                 }
@@ -427,6 +433,8 @@ public final class ColorDisplayService extends SystemService {
                     Secure.getUriFor(Secure.ACCESSIBILITY_DISPLAY_DALTONIZER_SATURATION_LEVEL),
                     false /* notifyForDescendants */, mContentObserver, mCurrentUser);
         }
+        cr.registerContentObserver(Secure.getUriFor(Secure.DISPLAY_ENGINE_MODE),
+                false /* notifyForDescendants */, mContentObserver, mCurrentUser);
 
         // Apply the accessibility settings first, since they override most other settings.
         onAccessibilityInversionChanged();
@@ -473,6 +481,10 @@ public final class ColorDisplayService extends SystemService {
                 onReduceBrightColorsActivationChanged(/*userInitiated*/ false);
                 mHandler.sendEmptyMessage(MSG_APPLY_REDUCE_BRIGHT_COLORS);
             }
+        }
+        
+        if (mDisplayEngineController.isAvailable(getContext())) {
+            mHandler.sendEmptyMessage(MSG_APPLY_UPDATE_DISPLAY_ENGINE);
         }
     }
 
@@ -1796,6 +1808,10 @@ public final class ColorDisplayService extends SystemService {
                     break;
                 case MSG_APPLY_DISPLAY_WHITE_BALANCE:
                     applyTintByCct(mDisplayWhiteBalanceTintController, false);
+                    break;
+                case MSG_APPLY_UPDATE_DISPLAY_ENGINE:
+                    mDisplayEngineController.updateBalance(getContext(), mCurrentUser);
+                    applyTint(mDisplayEngineController, true);
                     break;
             }
         }