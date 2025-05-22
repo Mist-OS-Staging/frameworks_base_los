77db9530c602f33faa87046146f1b1b19b4d8cdb
elpaablo <donopwhile1@gmail.com>
2025-05-22 15:35:50 +0100

    Disable night light when showing udfps overlay
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -41,6 +41,7 @@ import android.graphics.Rect;
 import android.hardware.biometrics.BiometricFingerprintConstants;
 import android.hardware.biometrics.BiometricPrompt;
 import android.hardware.biometrics.SensorProperties;
+import android.hardware.display.ColorDisplayManager;
 import android.hardware.display.DisplayManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.FingerprintSensorProperties;
@@ -233,6 +234,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
     private UdfpsAnimation mUdfpsAnimation;
 
+    private boolean mNightDisplayActivated;
+    private ColorDisplayManager mColorDisplayManager;
+
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
             new VibrationAttributes.Builder()
@@ -297,6 +301,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         @Override
         public void showUdfpsOverlay(long requestId, int sensorId, int reason,
                 @NonNull IUdfpsOverlayControllerCallback callback) {
+            maybeDisableNightMode();
             mUdfpsOverlayInteractor.setRequestId(requestId);
             mFgExecutor.execute(() -> UdfpsController.this.showUdfpsOverlay(
                     new UdfpsControllerOverlay(
@@ -336,6 +341,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         @Override
         public void hideUdfpsOverlay(int sensorId) {
+            maybeRestoreNightMode();
             mFgExecutor.execute(() -> {
                 if (mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
                     // if we get here, we expect keyguardUpdateMonitor's fingerprintRunningState
@@ -825,6 +831,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             updateUdfpsAnimation();
             mConfigurationController.addCallback(mConfigurationListener);
         }
+        mColorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
     }
 
     private void updateUdfpsAnimation() {
@@ -835,6 +842,19 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
         if (mUdfpsAnimation != null) {
             mUdfpsAnimation.updatePosition();
+        }
+    }
+
+    private void maybeDisableNightMode() {
+        mNightDisplayActivated = mColorDisplayManager.isNightDisplayActivated();
+        if (mNightDisplayActivated) {
+            mColorDisplayManager.setNightDisplayActivated(false);
+        }
+    }
+
+    private void maybeRestoreNightMode() {
+        if (mNightDisplayActivated) {
+            mColorDisplayManager.setNightDisplayActivated(true);
         }
     }
 
