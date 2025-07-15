6e06a19df5e67682a5552a8f1958611280e7bcd1
Dmitrii <bankersenator@gmail.com>
2025-07-15 20:35:54 +0000

    SystemUI: Hide progress chip during heads-up notifications
    Hide the progress chip when a heads-up notification is displayed
    to prevent UI overlap.
    
    The controller now listens to the HeadsUpManager to determine
    when a HUN is pinned.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1329,8 +1329,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                         mOnGoingActionProgressController =
                              new OnGoingActionProgressController(
                                      mContext,
-                                     statusBarViewController.getOngoingActionProgressGroup(),
-                                     mNotificationListener, mKeyguardStateController);
+                                     statusBarViewController.getOngoingActionProgressGroup(), mNotificationListener,
+                                     mKeyguardStateController, mHeadsUpManager);
                     });
         }
         if (!StatusBarRootModernization.isEnabled() && !StatusBarConnectedDisplays.isEnabled()) {
