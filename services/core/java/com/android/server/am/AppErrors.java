7d0780884bdcaa4108ae1978c1bbfafffa344811
Jyotiraditya Panda <jyotiraditya@aospa.co>
2025-08-11 13:38:24 +0000

    core: Switch to Memochō. https://bin.kv2.dev
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: Ie5331efc04b820937daa33eb99a9022fc4ce21dc

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -673,7 +673,7 @@ class AppErrors {
                 return;
             }
 
-            // Add paste content for dogbin option
+            // Add paste content for Memochō option
             data.paste = "time: " + timeMillis + "\n" +
             "msg: " + longMsg + "\n" +
             "stacktrace: " + stackTrace;
