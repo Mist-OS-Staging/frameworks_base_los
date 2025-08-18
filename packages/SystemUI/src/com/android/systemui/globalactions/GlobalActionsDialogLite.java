07c862e7bfbee3509c21f894f7a8da82f377ae9e
HELLBOY017 <abhaygill017@gmail.com>
2025-08-18 16:15:47 +0200

    SystemUI: GlobalActions: Don't show bugreport button
    Change-Id: Id5fb49becf187e41aa6ada1f76da3705c92a04bf

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -937,9 +937,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
 
     @VisibleForTesting
     boolean shouldDisplayBugReport(@Nullable UserInfo user) {
-        return user != null && user.isAdmin()
-                && mSecureSettings.getIntForUser(Settings.Secure.BUGREPORT_IN_POWER_MENU, 0,
-                user.id) != 0;
+        return false;
     }
 
     @Override
@@ -1463,14 +1461,12 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         }
 
         public boolean showDuringKeyguard() {
-            return true;
+            return false;
         }
 
         @Override
         public boolean showBeforeProvisioning() {
-            return Build.isDebuggable() && mSecureSettings.getIntForUser(
-                    Settings.Secure.BUGREPORT_IN_POWER_MENU, 0, getCurrentUser().id) != 0
-                    && getCurrentUser().isAdmin();
+            return false;
         }
     }
 
