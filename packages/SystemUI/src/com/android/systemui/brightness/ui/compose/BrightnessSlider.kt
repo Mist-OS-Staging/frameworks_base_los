ea4fafe8590df0341a5991e615410ec574a561aa
Pranav Vashi <neobuddy89@gmail.com>
2025-08-22 01:42:31 +0530

    SystemUI: Simplify autobrightness and haptics toggle in brightness slider
    Change-Id: I5783268475cad2ef734c7bb00d645545e8d36651
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
+++ packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
@@ -19,6 +19,7 @@ package com.android.systemui.brightness.ui.compose
 import android.content.Context
 import android.graphics.PorterDuff
 import android.provider.Settings
+import android.os.UserHandle
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.MotionEvent
@@ -120,7 +121,6 @@ import com.android.systemui.haptics.slider.compose.ui.SliderHapticsViewModel
 import com.android.systemui.lifecycle.rememberViewModel
 import com.android.systemui.qs.ui.compose.borderOnFocus
 import com.android.systemui.res.R
-import com.android.systemui.tuner.TunerService
 import com.android.systemui.utils.PolicyRestriction
 import platform.test.motion.compose.values.MotionTestValueKey
 import platform.test.motion.compose.values.motionTestValues
@@ -145,29 +145,28 @@ fun BrightnessSlider(
     modifier: Modifier = Modifier,
     showToast: () -> Unit = {},
     hapticsViewModelFactory: SliderHapticsViewModel.Factory,
-    tunerService: TunerService,
 ) {
-    val QS_SHOW_AUTO_BRIGHTNESS =
-        "lineagesecure:" + LineageSettings.Secure.QS_SHOW_AUTO_BRIGHTNESS
-    val QS_BRIGHTNESS_SLIDER_HAPTIC =
-        "system:" + Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC
+    val context = LocalContext.current
+    val cr = context.contentResolver
 
     val showAutoBrightness = remember {
-        mutableStateOf(
-            TunerService.parseIntegerSwitch(
-                tunerService.getValue(QS_SHOW_AUTO_BRIGHTNESS),
-                true
-            )
-        )
+        try {
+            LineageSettings.Secure.getIntForUser(
+                cr, LineageSettings.Secure.QS_SHOW_AUTO_BRIGHTNESS, 1, UserHandle.USER_CURRENT
+            ) != 0
+        } catch (_: Throwable) {
+            false
+        }
     }
 
     val hapticsEnabled = remember {
-        mutableStateOf(
-            TunerService.parseIntegerSwitch(
-                tunerService.getValue(QS_BRIGHTNESS_SLIDER_HAPTIC),
-                false
-            )
-        )
+        try {
+            Settings.System.getIntForUser(
+                cr, Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC, 0, UserHandle.USER_CURRENT
+            ) != 0
+        } catch (_: Throwable) {
+            false
+        }
     }
 
     var value by remember(gammaValue) { mutableIntStateOf(gammaValue) }
@@ -178,7 +177,7 @@ fun BrightnessSlider(
     val enabled = !isRestricted
     val interactionSource = remember { MutableInteractionSource() }
     val hapticsViewModel: SliderHapticsViewModel? =
-        if (hapticsEnabled.value) {
+        if (hapticsEnabled) {
             rememberViewModel(traceName = "SliderHapticsViewModel") {
                 hapticsViewModelFactory.create(
                     interactionSource,
@@ -206,7 +205,6 @@ fun BrightnessSlider(
                 iconResProvider(percentage)
             }
         }
-    val context = LocalContext.current
     val painter: Painter by
         produceState<Painter>(
             initialValue = ColorPainter(Color.Transparent),
@@ -237,24 +235,6 @@ fun BrightnessSlider(
 
     val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
     val EFFECT_CLICK = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK)
-
-    DisposableEffect(Unit) {
-        val tunable = TunerService.Tunable { key, newValue ->
-            if (key == QS_SHOW_AUTO_BRIGHTNESS) {
-                showAutoBrightness.value =
-                    TunerService.parseIntegerSwitch(newValue, true)
-            } else if (key == QS_BRIGHTNESS_SLIDER_HAPTIC) {
-                hapticsEnabled.value =
-                    TunerService.parseIntegerSwitch(newValue, false)
-            }
-        }
-        tunerService.addTunable(tunable, QS_SHOW_AUTO_BRIGHTNESS)
-        tunerService.addTunable(tunable, QS_BRIGHTNESS_SLIDER_HAPTIC)
-
-        onDispose {
-            tunerService.removeTunable(tunable)
-        }
-    }
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -384,7 +364,7 @@ fun BrightnessSlider(
             }
         )
 
-        if (hasAutoBrightness && showAutoBrightness.value) {
+        if (hasAutoBrightness && showAutoBrightness) {
             Spacer(modifier = Modifier.width(10.dp))
 
             val coroutineScope = rememberCoroutineScope()
@@ -535,7 +515,6 @@ fun BrightnessSliderContainer(
             showToast = {
                 viewModel.showToast(context, R.string.quick_settings_brightness_unable_adjust_msg)
             },
-            tunerService = viewModel.tunerService,
         )
     }
 }
