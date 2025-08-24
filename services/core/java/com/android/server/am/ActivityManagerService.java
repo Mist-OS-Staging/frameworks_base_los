f4100bfdc4c2f132984497b50b1d1dd656571a7d
ShukakuZa <yusufzuzu787@gmail.com>
2025-08-24 16:47:57 +0000

    base: fixup duplicate of SwipeToScreenshort

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19366,42 +19366,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
     }
 
-    private class SwipeToScreenshotObserver extends ContentObserver {
-
-        private final Context mContext;
-
-        public SwipeToScreenshotObserver(Handler handler, Context context) {
-            super(handler);
-            mContext = context;
-        }
-
-        public void registerObserver() {
-            mContext.getContentResolver().registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.THREE_FINGER_GESTURE),
-                    false, this, UserHandle.USER_ALL);
-            update();
-        }
-
-        private void update() {
-            mIsSwipeToScreenshotEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
-        }
-
-        public void onChange(boolean selfChange) {
-            update();
-        }
-    }
-
-    @Override
-    public boolean isSwipeToScreenshotGestureActive() {
-        return mIsSwipeToScreenshotEnabled && mIsSwipeToScreenshotActive;
-    }
-
-    @Override
-    public void setSwipeToScreenshotGestureActive(boolean enabled) {
-         mIsSwipeToScreenshotActive = enabled;
-    }
-
     /**
      * Deal with binder transactions to frozen apps.
      *
