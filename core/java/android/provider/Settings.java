0d15621fa0442d71a245aceaf7261c2e48d18429
Pranav Vashi <neobuddy89@gmail.com>
2025-09-02 09:23:08 +0000

    SystemUI: Add pixel style lockscreen weather based OmniJaws * Similar to SmartSpace but more consistent with keyguard status area. * Complete rewrite of omni implementation on Android 11.
    Ref:
    9773423
    05fae49
    8a923c2
    
    Change-Id: I138c0dc94f08142f6614659037a501d6ae8909b1
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14212,6 +14212,26 @@ public final class Settings {
         public static final String LOCK_SCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_TEXT = "lockscreen_weather_text";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_WIND_INFO = "lockscreen_weather_wind_info";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_HUMIDITY_INFO = "lockscreen_weather_humidity_info";
+
+        /**
          * Whether the feature that the device will fire a haptic when users scroll and hit
          * the edge of the screen is enabled.
          *
