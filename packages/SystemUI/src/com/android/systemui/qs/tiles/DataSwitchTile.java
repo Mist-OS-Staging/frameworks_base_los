81688fda3cf5e24ee2fac603587e04f2702adc2f
Joey Huab <jhuab@edukasyon.ph>
2025-09-17 11:13:46 +0900

    SystemUI: Switch custom QS tiles metrics to `QS_PANEL`

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -234,7 +234,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.MIST;
+        return MetricsEvent.QS_PANEL;
     }
 
     /**
