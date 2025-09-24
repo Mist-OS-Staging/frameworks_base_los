5171de8e3cb873fcc8329adb7c3233e3ff9c2a81
Joey <jhuab@edukasyon.ph>
2025-09-24 23:36:57 +0900

    core: Move *Utils files to evolution directory

--- packages/SystemUI/src/com/android/systemui/statusbar/OnGoingActionProgressController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/OnGoingActionProgressController.java
@@ -52,7 +52,7 @@ import com.android.systemui.statusbar.OnGoingActionProgressGroup;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.util.MediaSessionManagerHelper;
 
-import com.android.internal.util.android.VibrationUtils;
+import com.android.internal.util.evolution.VibrationUtils;
 
 import java.util.HashMap;
 import java.util.concurrent.Executor;
