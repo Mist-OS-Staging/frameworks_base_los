CherishOS/platform_frameworks_base

fixup! Initial implementation of KeyboxImitationHooks
Co-authored-by: chiteroman <querveros999@gmail.com>
Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>

core/java/com/android/internal/util/cherish/PixelPropsUtils.java
@@ -42,6 +42,7 @@ import android.view.WindowManager;
 
 import com.android.internal.R;
 import com.android.internal.util.cherish.CherishUtils;
+import com.android.internal.util.cherish.KeyProviderManager;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -767,6 +768,11 @@ public final class PixelPropsUtils {
     }
 
     public static void onEngineGetCertificateChain() {
+        // If a keybox is found, don't block key attestation
+        if (KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
+            return;
+        }
         boolean isPixelGmsEnabled = SystemProperties.getBoolean(SPOOF_PIXEL_GMS, false);
         if (!isPixelGmsEnabled)
             return;

Mist-OS-Staging/frameworks_base_los

core/java/com/android/internal/util/mist/PixelPropsUtils.java
@@ -42,6 +42,7 @@ import android.view.WindowManager;
 
 import com.android.internal.R;
 import com.android.internal.util.mist.Utils;
+import com.android.internal.util.mist.KeyProviderManager;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -772,6 +773,11 @@ public final class PixelPropsUtils {
     }
 
     public static void onEngineGetCertificateChain() {
+        // If a keybox is found, don't block key attestation
+        if (KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
+            return;
+        }
         boolean isPixelGmsEnabled = SystemProperties.getBoolean(SPOOF_PIXEL_GMS, true);
         if (!isPixelGmsEnabled)
             return;