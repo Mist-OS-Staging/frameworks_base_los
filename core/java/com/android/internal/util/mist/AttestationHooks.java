0d3110414872bc1e8f9a2fce8dbce8269184de0e
Pranav Vashi <neobuddy89@gmail.com>
2025-10-03 09:22:57 +0000

    PixelPropsUtils: Spoof netflix to komodo - From: https://github.com/crdroidandroid/android_frameworks_base/blob/c85efefcaa602584f94ec081710b7d4dcdede401/core/java/com/android/internal/util/crdroid/PixelPropsUtils.java#L247
    Change-Id: I5ae94174e44bf8136739442e2c64e6752068eb2b

--- core/java/com/android/internal/util/mist/AttestationHooks.java
+++ core/java/com/android/internal/util/mist/AttestationHooks.java
@@ -43,6 +43,7 @@ public final class AttestationHooks {
 
     private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
     private static final String PACKAGE_SNAPCHAT = "com.snapchat.android";
+    private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
 
     private static final String SPOOF_PIXEL_GPHOTOS = "persist.sys.pixelprops.gphotos";
     private static final String SPOOF_PIXEL_SNAPCHAT = "persist.sys.pixelprops.snap";
@@ -93,6 +94,12 @@ public final class AttestationHooks {
         if (packageName.equals(PACKAGE_SNAPCHAT)) {
             if (SystemProperties.getBoolean(SPOOF_PIXEL_SNAPCHAT, false)) {
                 sPixelXLProps.forEach(AttestationHooks::setPropValue);
+            }
+        }
+
+        if (packageName.equals(PACKAGE_NETFLIX)) {
+            if (SystemProperties.getBoolean(SPOOF_PIXEL_NETFLIX, false)) {
+                sMainlineProps.forEach(AttestationHooks::setPropValue);
             }
         }
     }
